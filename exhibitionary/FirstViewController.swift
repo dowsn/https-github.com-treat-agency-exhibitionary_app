2edcc4997a8d087a6b21228ccf25bf7d4f0fb893
Istvan Szilagyi <istvanszilagyi@luigi.local>
2016-03-13 22:54:40 +0000

    Initial Commit

---
+++ exhibitionary/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  exhibitionary
+//
+//  Created by Istvan Szilagyi on 13/03/16.
+//  Copyright © 2016 Such Company LLC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ exhibitionary/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  exhibitionary
+//
+//  Created by Istvan Szilagyi on 13/03/16.
+//  Copyright © 2016 Such Company LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
